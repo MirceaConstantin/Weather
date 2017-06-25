a2baf2cc1b13bd4b6d5ce4b1ce146d0886b75225
andrei828 <andrei.liviu10@gmail.com>
2017-06-25 12:40:12 +0300

    Python stuff

---
+++ Python files/Date.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET 
+
+try:
+	from urllib.request import urlopen
+except ImportError:
+	from urllib2 import urlopen
+
+xml_url = "http://meteo.arso.gov.si/uploads/probase/www/fproduct/text/en/forecast_BUCUR-BAN_latest.xml"
+
+url = urlopen(xml_url).read()
+xml = ET.fromstring(url)
+
+Date = []
+for day in xml.findall('metData'):
+	Date.append((day.find('valid').text.split(' ')[0], day.find('valid_day').text.split(' ')[0]))
+
+print(Date)
+
