a8f2d0c10e309afe3552291b9cd5ec050fe71f37
andrei828 <andrei.liviu10@gmail.com>
2017-06-27 11:48:52 +0300

    Python files

--- Python files/GetData.py
+++ Python files/GetData.py
@@ -1,5 +1,4 @@
 import xml.etree.ElementTree as ET
-
 try:
 	from urllib.request import urlopen
 except ImportError:
@@ -19,15 +18,6 @@ MinMaxTemperature = []
 WindDirection = []
 WindSpeed = []
 
-# XML file iteration
-for Data in Iter:
-	Date.append((Data.find('valid').text.split(' ')[0], Data.find('valid_day').text.split(' ')[0]))
-	Hours.append((Data.find('sunrise').text.split(' ')[1], Data.find('sunset').text.split(' ')[1]))
-	weatherType.append(Data.find('nn_shortText').text)
-	MinMaxTemperature.append((Data.find('txsyn').text, Data.find('tnsyn').text))
-	WindDirection.append(Data.find('dd_shortText').text)
-	WindSpeed.append(int(Data.find('ff_value').text) * 3.6)
-
 # Temperature exctracion (found only in the first <metData>)
 Temperature = Search.find('t').text
 
@@ -38,17 +28,20 @@ xml_Humidity = ET.fromstring(url_Humidity)
 Humidity_Search = xml_Humidity.find('metData')
 Humidity = Humidity_Search.find("rh").text
 
-
+# Writing data to the file
 with open("Get_Data_For_App.txt", 'w') as file_out:
-	for i in range(0, 5):
-		file_out.write(str(Date[i][0])+'\n')
-		file_out.write(str(Date[i][1])+'\n')
-		file_out.write(str(Hours[i][0])+'\n')
-		file_out.write(str(Hours[i][1])+'\n')
-		file_out.write(str(weatherType[i])+'\n')
-		file_out.write(Temperature+'\n')
-		file_out.write(str(MinMaxTemperature[i][0])+'\n')
-		file_out.write(str(MinMaxTemperature[i][1])+'\n')
-		file_out.write(Humidity+'\n')
-		file_out.write(str(WindDirection[i])+'\n')
-		file_out.write(str(WindSpeed[i])+'\n')
+	# Temperature
+	file_out.write(Temperature+'\n')
+	# Humidity
+	file_out.write(Humidity+'\n')
+	# XML file iteration
+	for Data in Iter:
+		file_out.write(Data.find('valid').text.split(' ')[0]+'\n')			# Date
+		file_out.write(Data.find('valid_day').text.split(' ')[0]+'\n')		# Week Date
+		file_out.write(Data.find('sunrise').text.split(' ')[1]+'\n')		# Sunrise 
+		file_out.write(Data.find('sunset').text.split(' ')[1]+'\n')			# Sunset
+		file_out.write(Data.find('nn_shortText').text+'\n')					# Weather type
+		file_out.write(Data.find('txsyn').text+'\n')						# Max temperature
+		file_out.write(Data.find('tnsyn').text+'\n')						# Min Temperature
+		file_out.write(Data.find('dd_shortText').text+'\n')					# Wind direction
+		file_out.write(str(int(Data.find('ff_value').text) * 3.6)+'\n')		# Wind speed
